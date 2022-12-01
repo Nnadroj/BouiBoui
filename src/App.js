83316d4ea9e1e824cf17db54c1d97037ca429e75
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-12-01 14:32:07 +0100

    geoloc test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Geolocalization from "./components/Geolocalization";
 
 function App() {
   return (
     <div className="App">
       <h1> BOUIBOUI</h1>
+      <Geolocalization />
     </div>
   );
 }
