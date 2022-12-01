cf1c00993c0dd679ec06e18f82b5203b01444316
Quentin Feuvrais <feuvrais.quentin@gmail.com>
2022-12-01 15:30:03 +0100

    merge

--- src/services/RestaurantData.jsx
+++ src/services/RestaurantData.jsx
@@ -1,17 +1,17 @@
 const restaurant = [
-    {
-        name: "Chez Bob",
-        street: "rue des bains",
-        postCode: "17420",
-        city: "Saint-Palais-sur-Mer",
-        country: "France"
-    },
-    {
-        name: "Burger Lutèce",
-        street: "30 rue du surmelin",
-        postCode: "75020",
-        city: "Paris",
-        country: "France"
-    }
-]
+  {
+    name: "Chez Bob",
+    street: "rue des bains",
+    postCode: "17420",
+    city: "Saint-Palais-sur-Mer",
+    country: "France",
+  },
+  {
+    name: "Burger Lutèce",
+    street: "30 rue du surmelin",
+    postCode: "75020",
+    city: "Paris",
+    country: "France",
+  },
+];
 export default restaurant;
