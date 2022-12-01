f25d4834d89e172c37a54c1fa31917bdf1405efd
Jordann <jordannlegal.contact@gmail.com>
2022-12-01 14:05:45 +0100

    update on the skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Page1 from "./Pages/Page1";
+import Page2 from "./Pages/Page3";
+import Page3 from "./Pages/Page3";
 
 function App() {
   return (
     <div className="App">
-      <h1> BOUIBOUI</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
+      {/*  */}
     </div>
   );
 }
---
+++ src/components/Page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Page.css";
+
+function Page() {
+  return <div className="page">Page component</div>;
+}
+
+export default Page;
