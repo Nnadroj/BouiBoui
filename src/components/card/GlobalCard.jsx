42dd1fac469ef6dc0b4041d58d45bf3e80275f9d
Jordann <jordannlegal.contact@gmail.com>
2022-12-02 00:33:35 +0100

    udpating the page3 behavior

---
+++ src/components/card/GlobalCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GlobalCard() {
+  return <div>GlobalCard</div>;
+}
+
+export default GlobalCard;
--- src/services/RestaurantData.jsx
+++ src/services/RestaurantData.jsx
@@ -1,47 +1,83 @@
 const restaurants = [
   {
     name: "Chez Bob",
+    category: "Brasserie",
     street: "rue des bains",
     postCode: "17420",
     city: "Saint-Palais-sur-Mer",
     country: "France",
     coords: {
-        latitude: 45.64247,
-        longitude: -1.08665,
+      latitude: 45.64247,
+      longitude: -1.08665,
     },
-    offer: [{
-      title: "offer",
-      data: "offer data",
-      picture: "offer-picture"
-    }]
+    subscription_date: "",
+    offer: [
+      {
+        title: "offer",
+        data: "offer data",
+        picture: "offer-picture",
+      },
+    ],
   },
   {
     name: "Burger Lutèce",
+    category: "Burger",
     street: "30 rue du surmelin",
     postCode: "75020",
     city: "Paris",
     country: "France",
     coords: {
-        latitude: 48.86831,
-        longitude: 2.40398,
+      latitude: 48.86831,
+      longitude: 2.40398,
     },
-    offer: [{
-      title: "offer",
-      data: "offer data",
-      picture: "offer-picture"
-    }]
+    subscription_date: 3,
+    offer: [
+      /*  {
+        title: "offer",
+        data: "offer data",
+        picture: "offer-picture",
+      }, */
+    ],
   },
   {
     name: "La confidente",
+    category: "Francais moderne",
     street: "125 avenue Gambetta",
     postCode: "75020",
     city: "Paris",
     country: "France",
     coords: {
-        latitude: 48.86830,
-        longitude: 2.40116,
+      latitude: 48.8683,
+      longitude: 2.40116,
     },
-    offer: []
+    subscription_date: 8,
+    offer: [
+      {
+        title: "offer",
+        data: "offer data",
+        picture: "offer-picture",
+      },
+    ],
+  },
+  {
+    name: "Chang Sheng",
+    category: "Chinois",
+    street: "95 Av. Gambetta",
+    postCode: "75020",
+    city: "Paris",
+    country: "France",
+    coords: {
+      latitude: 48.8665182,
+      longitude: 2.3997201,
+    },
+    subscription_date: 8,
+    offer: [
+      {
+        title: "offer",
+        data: "offer data",
+        picture: "offer-picture",
+      },
+    ],
   },
 ];
 export default restaurants;
